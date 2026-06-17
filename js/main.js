257baa12c7576730e741f7f2ab1215aef0116f8e
Usman <uthmahamzah1977@gmail.com>
2026-06-17 08:56:49 +0100

    updates

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@ function tsr() {
   document.body.innerHTML = "what?";
   console.log("did it work? Yes, yes it did.");
 }
+
+let users = [
+  {
+    name: "Usman",
+    age: 19,
+    isStudent: true,
+  },
+  {
+    name: "Usman",
+    age: 19,
+    isStudent: true,
+  },
+];
+
+for (let user of users) {
+  for (let key in user) {
+    console.log(key.valueOf());
+  }
+}
