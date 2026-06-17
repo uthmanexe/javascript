a728b2d8c1e75813f4cc56c74062eddd1d51d7c3
Usman <uthmahamzah1977@gmail.com>
2026-06-17 15:16:06 +0100

    ...

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// // console.log("Hello")
+// // console.log("I like pizza")
+// // // window.alert("This is an alert!")
+// // document.getElementById("h1").textContent = 'Hello';
+// let x = 123;
+// let name = "Usman"
+// let age = 18
+
+// document.getElementById("h1").textContent = name
+// document.getElementById("p1").textContent = `${age} years old`;
+// document.getElementById("p2").textContent = `${x} is greater than 20`;
